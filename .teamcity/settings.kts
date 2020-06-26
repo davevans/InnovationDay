4f04ce700dfe068031c1e735ddd40acdb2400ff5
Dav Evans <dav@dav-evans.com>
2020-06-26 15:44:25 +1000

    generate subprojects

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,123 +3,51 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import java.io.File
 import java.util.*
 
-data class TeamCityProject(val id: String, val name: String, val parentProjectName: String?)
+//data class TeamCityProject(val id: String, val name: String, val parentProjectName: String?)
+
+// Model
+class TeamCityProject(val name: String) {
+    var parent: TeamCityProject? = null
+}
 
 version = "2020.1"
 
 project {
 
-    //template(MyTemplate)
-    //buildType(HelloWorld)
-
 
     /* Read yml files to create subprojects */
     var projectsRoot = File(DslContext.baseDir, "/Projects")
-    var subProjects = discoverSubProjects(projectsRoot)
-    for (p: TeamCityProject in subProjects) {
+    var subdirectories = projectsRoot.listFiles()
+    for (item: File in subdirectories)
+    {
+        if(item.isDirectory())
+        {
+            var proj = getProject(item, null);
+            println("adding ${proj!!.name}")
 
-        println("Adding project ${p.name}. ParentName: ${p.parentProjectName}")
-        //var id = id(p.id);
-        //var parentId = if(p.parentProjectName != null) AbsoluteId(p.parentProjectName.toId()) else null
-
-        subProject({
-            id(p.id)
-            name = p.name
-            uuid = p.name
-        })
+            subProject({
+                id(proj!!.name)
+                name = proj.name
+            })
+        }
     }
 }
 
-fun discoverSubProjects(dir: File) : List<TeamCityProject> {
-    var result = LinkedList<TeamCityProject>()
-    var projects = LinkedList<File>()
-    var subdirectories = dir.listFiles()
+fun getProject(currentDirectory: File, parent: TeamCityProject?) : TeamCityProject? {
+
+    var proj = TeamCityProject(currentDirectory.name)
+    proj.parent = parent
+
+    println("discovered ${proj.name}")
+
+    var subdirectories = currentDirectory.listFiles()
     for (item: File in subdirectories) {
         if(item.isDirectory())
         {
-            projects.add(item);
-            var projectFile = getProjectFile(item);
-            if(projectFile != null)
-            {
-                var subProject = getTeamCityProject(projectFile, null)
-                result.add(subProject)
-            }
+            getProject(item, proj);
         }
     }
 
-    while (!projects.isEmpty())
-    {
-        val parentDirectory: File = projects.poll()
-        var parentProjFile = getProjectFile(parentDirectory);
-
-        var parentTeamCityProj = getTeamCityProject(parentProjFile!!, null)
-        subdirectories = parentDirectory.listFiles()
-        for (item: File in subdirectories) {
-            if(item.isDirectory())
-            {
-                projects.add(item);
-                var projectFile = getProjectFile(item);
-                if(projectFile != null)
-                {
-                    var subProject = getTeamCityProject(projectFile, parentTeamCityProj.name)
-                    result.add(subProject)
-                }
-            }
-        }
-    }
-    return result
+    return proj;
 }
 
-fun getProjectFile(dir: File) : File? {
-    if(!dir.isDirectory())
-        return null
-
-    val projectFile = dir.listFiles { f -> f.isFile() && f.name == "_Project.yml" }.firstOrNull();
-    if(projectFile != null)
-        return projectFile
-
-    return null
-}
-
-fun getTeamCityProject(file: File, parentProjectName: String?) : TeamCityProject {
-    var fileContent = file.readText()
-    /*
-    var sub = Project({
-        id(file.parentFile.name)
-        name = file.parentFile.name
-    })
-    */
-
-    var name = file.parentFile.name
-    var teamCityProj = TeamCityProject(name, name, parentProjectName)
-
-    //println("Creating subProject with name ${sub.name}.")
-    return teamCityProj
-
-    //var serializer = TeamCityProject.serializer()
-    //var project: TeamCityProject = Yaml.default.parse(TeamCityProject.serializer(), fileContent)
-}
-
-
-object HelloWorld: BuildType({
-    name = "Hello World"
-    templates(MyTemplate)
-
-    params {
-        param("repository", "Hello World")
-    }
-})
-
-object MyTemplate: Template({
-    name = "MyTemplate"
-
-    steps  {
-        script {
-            scriptContent = "echo 'hello world' lets compile %repository%"
-        }
-    }
-
-    params {
-        param("repository", "")
-    }
-})
